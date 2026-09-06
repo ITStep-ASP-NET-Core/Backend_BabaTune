c4dce9fe0fecdfb3beb68a668961bd52b79ea4b0
CutoffSquire917 <pushkin080109@gmail.com>
2026-09-07 00:15:18 +0200

    Initial commit

---
+++ BabaTune.WebApi/Program.cs
@@ -0,0 +1,24 @@
+
+namespace BabaTune.WebApi
+{
+	public class Program
+	{
+		public static void Main ( string[] args )
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			builder.Services.AddControllers();
+
+			var app = builder.Build();
+
+			app.UseHttpsRedirection();
+
+			app.UseAuthorization();
+
+
+			app.MapControllers();
+
+			app.Run();
+		}
+	}
+}
