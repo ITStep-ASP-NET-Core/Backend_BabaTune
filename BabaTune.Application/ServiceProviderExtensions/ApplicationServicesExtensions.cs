65ed2c4da0ca6d2e651c0c0aa54a2f3b3a6aa317
CutoffSquire917 <pushkin080109@gmail.com>
2026-09-25 12:10:06 +0200

    feat(application): add DI registration extensions

---
+++ BabaTune.Application/ServiceProviderExtensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,24 @@
+﻿using BabaTune.Application.Implementations;
+using BabaTune.Application.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BabaTune.Application.ServiceProviderExtensions
+{
+    public static class ApplicationServicesExtensions
+	{
+        public static void AddApplicationServices ( this IServiceCollection services )
+		{
+			services.AddScoped<IPasswordHasher, Argon2PasswordHasher>();
+			services.AddScoped<IAuthService, AuthService>();
+			services.AddScoped<IUserService, UserService>();
+			services.AddScoped<ISongService, SongService>();
+			services.AddScoped<IAlbumService, AlbumService>();
+			services.AddScoped<IPlaylistService, PlaylistService>();
+			services.AddScoped<ICategoryService, CategoryService>();
+			services.AddScoped<IGenreService, GenreService>();
+			services.AddScoped<INoticeService, NoticeService>();
+			services.AddScoped<ISubscribeService, SubscribeService>();
+			services.AddScoped<IListenHistoryService, ListenHistoryService>();
+		}
+	}
+}
